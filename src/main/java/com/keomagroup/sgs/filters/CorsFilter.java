528bf28e513cd9cc0c9262121e41f23704dfc583
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-08-15 13:16:38 +0200

    Cors enabled

--- src/main/java/com/keomagroup/sgs/filters/CorsFilter.java
+++ src/main/java/com/keomagroup/sgs/filters/CorsFilter.java
@@ -1,15 +1,16 @@
 package com.keomagroup.sgs.filters;
 
 import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+
 
 public class CorsFilter extends HttpFilter implements Filter {
        
@@ -19,10 +20,17 @@ public class CorsFilter extends HttpFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
-		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("Access-Control-Allow-Origin", "*"); 
-		res.addHeader("Access-Control-Allow-Headers", "*");
-		res.addHeader("Access-Control-Allow-Methods", "*");
+		if(response instanceof HttpServletResponse){
+            HttpServletResponse res = (HttpServletResponse) response;
+            System.out.println("Correu aqui dentro");
+            
+            res.addHeader("Access-Control-Allow-Origin", "*"); 
+    		res.addHeader("Access-Control-Allow-Headers", "*");
+    		res.addHeader("Access-Control-Allow-Credentials", "true");
+    		res.addHeader("Access-Control-Allow-Methods", "*");
+        }else {
+        	System.out.println("Ocorreu um erro Qualquer");
+        }
 		
 		
 		chain.doFilter(request, response);
